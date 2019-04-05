39fb2b8974bf68dac040bef81633f984aa4153e8
Abdulelah Alshalhoub <abdulelah.sh00@gmail.com>
2019-04-05 13:51:42 +0300

    created: addBlock, getBlock...

--- BlockChain.js
+++ BlockChain.js
@@ -17,23 +17,68 @@ class Blockchain {
     // You have to options, because the method will always execute when you create your blockchain
     // you will need to set this up statically or instead you can verify if the height !== 0 then you
     // will not create the genesis block
-    generateGenesisBlock(){
-        // Add your code here
+    generateGenesisBlock() {
+        this.getBlockHeight().then((count) => {
+            if (count == 0) {
+                block.height = count;
+                block.time = new Date().getTime().toString().slice(0, -3);
+                block.hash = SHA256(JSON.stringify(block)).toString();
+                this.bd.addLevelDBData(count, block).then((result) => {
+                    console.log(`Genesis block created: ${result}`);
+                }).catch((err) => {
+                    console.log(err);
+                });
+            }
+        }).catch((err) => {
+            console.log(err);
+        });
     }
 
     // Get block height, it is a helper method that return the height of the blockchain
     getBlockHeight() {
-        // Add your code here
+        return new Promise(function (resolve, reject) {
+            this.bd.getBlocksCount().then((count) => {
+                resolve(count);
+            }).catch((err) => {
+                reject(err);
+            });
+        });
     }
 
     // Add new block
     addBlock(block) {
-        // Add your code here
+        return new Promise(function (resolve, reject) {
+            this.getBlockHeight().then((count) => {
+                block.height = count;
+                block.time = new Date().getTime().toString().slice(0, -3);
+                this.getBlock(count - 1).then((previousBlock) => {
+                    block.previousBlockHash = previousBlock.hash;
+                    block.hash = SHA256(JSON.stringify(block)).toString();
+                    this.bd.addLevelDBData(count, block).then((result) => {
+                        resolve(result);
+                    }).catch((err) => {
+                        reject(err);
+                    });
+                }).catch((err) => {
+                    reject(err);
+                });
+            }).catch((err) => {
+                reject(err);
+            });
+        });
+
     }
 
     // Get Block By Height
     getBlock(height) {
         // Add your code here
+        return new Promise(function (resolve, reject) {
+            this.bd.getLevelDBData(height).then((block) => {
+                resolve(block);
+            }).catch((err) => {
+                reject(err);
+            });
+        });
     }
 
     // Validate if Block is being tampered by Block Height
@@ -50,13 +95,16 @@ class Blockchain {
     // This method is for testing purpose
     _modifyBlock(height, block) {
         let self = this;
-        return new Promise( (resolve, reject) => {
+        return new Promise((resolve, reject) => {
             self.bd.addLevelDBData(height, JSON.stringify(block).toString()).then((blockModified) => {
                 resolve(blockModified);
-            }).catch((err) => { console.log(err); reject(err)});
+            }).catch((err) => {
+                console.log(err);
+                reject(err)
+            });
         });
     }
-   
+
 }
 
 module.exports.Blockchain = Blockchain;
